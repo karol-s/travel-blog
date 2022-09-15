45f33860225bedfc594974843a737185efd7a0b4
Karol <karolsurmanski13@gmail.com>
2022-09-15 09:47:46 +0200

    Uncomment db.create_all()

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class Comment(db.Model):
     comment_author = relationship("User", back_populates="comments")
     parent_post = relationship("BlogPost", back_populates="comments")
     
-# db.create_all()
+db.create_all()
 
 def admin_only(func):
     @wraps(func)
